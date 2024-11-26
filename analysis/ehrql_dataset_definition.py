1033a05eb769e4fe1187662ac1f6827aa2334d6d
Michael Chapman <788026+michaelchapman@users.noreply.github.com>
2024-11-26 07:19:40 +0000

    ehrql_dataset_definition for female patients

--- analysis/ehrql_dataset_definition.py
+++ analysis/ehrql_dataset_definition.py
@@ -6,7 +6,7 @@ dataset = create_dataset()
 age = patients.age_on("2020-03-31")
 first_event_date = clinical_events.sort_by(clinical_events.date).first_for_patient().date
 
-dataset.define_population((age > 18) & (age < 80))
+dataset.define_population((age > 18) & (age < 80) & (patients.sex = "female")
 dataset.age = age
 dataset.sex = patients.sex
 dataset.first_event_date = first_event_date
